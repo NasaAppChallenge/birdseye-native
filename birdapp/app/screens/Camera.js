9dc6ad039bf25f4364e0ef8c5f6b4db0268068d1
Alex <alex@Cmeds-MacBook-Pro.local>
2017-04-29 14:21:30 +0300

    added upload functionality

--- birdapp/app/screens/Camera.js
+++ birdapp/app/screens/Camera.js
@@ -8,8 +8,16 @@ import {
   View
 } from 'react-native';
 import Camera from 'react-native-camera';
+import RNFetchBlob from 'react-native-fetch-blob';
 
+const protocol = 'https';
+const host = 'birdseye.space';
 
+const api_endpoints = {
+  media: 'v1/media',
+  observation: 'v1/observations'
+
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -32,6 +40,13 @@ const styles = StyleSheet.create({
 
 
 export default class CameraScreen extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      img: null
+    }
+  }
   render() {
     return (
       <View style={styles.container}>
@@ -40,7 +55,8 @@ export default class CameraScreen extends Component {
             this.camera = cam;
           }}
           style={styles.preview}
-          aspect={Camera.constants.Aspect.fill}>
+          aspect={Camera.constants.Aspect.fill}
+          captureTarget={Camera.constants.CaptureTarget.disk}>
           <TouchableHighlight
             onPress={this.takePicture.bind(this)}>
             <Text style={styles.capture}>
@@ -51,12 +67,35 @@ export default class CameraScreen extends Component {
       </View>
     );
   }
-
+  uploadPicture(base64Image) {
+    const url = `${protocol}://${host}/${api_endpoints.media}`
+    console.log(url);
+    RNFetchBlob.fetch('POST', url, {
+        'Content-Type' : 'application/octet-stream',
+      }, base64Image)
+      .then((res) => {
+        console.log(res.data)
+      })
+      .catch((err) => {
+        // error handling ..
+      })
+  }
   takePicture() {
     const options = {};
     //options.location = ...
     this.camera.capture({metadata: options})
-      .then((data) => console.log(data))
+      .then(
+        (data) => {
+          RNFetchBlob.fs.readFile(data.path, 'base64').then(
+            (base64data) => {
+              let base64Image = `data:image/jpeg;base64,${base64data}`;
+              this.setState({img: base64Image});
+              //console.log(base64Image);
+              this.uploadPicture(base64Image);
+            }
+          )
+        }
+    )
       .catch(err => console.error(err));
   }
 }
